41948530d2d1ad2c3fe0eddb3a6c0be9f3235077
bfren <ben@bcgdesign.com>
2022-02-11 16:36:46 +0000

    v4.0.0 (#74)
    Major updates
    * **Breaking changes** major rewrite of S6 Overlay for v3.0.0.0 - see base image and documentation for more information
    
    Minor updates
    * Moving PHP_DIR variable to Dockerfile
    
    Build updates
    * Adding generate-dockerfiles.sh
    
    Documentation updates
    * Updating copyright year to 2022

---
+++ generate-dockerfiles.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker pull bfren/alpine
+
+BASE_REVISION="3.0.3"
+echo "Base: ${BASE_REVISION}"
+
+PHP_VERSIONS="7.3 7.4 7.4-edge 8.0 8.0-edge"
+for V in ${PHP_VERSIONS} ; do
+
+    echo "PHP ${V}"
+    PHP_MAJOR="$(echo ${V} | cut -c 1)"
+    NGINX_BASE=`cat ./${V}/NGINX_BASE`
+
+    DOCKERFILE=$(docker run \
+        -v ${PWD}:/ws \
+        -e BF_DEBUG=0 \
+        bfren/alpine esh \
+        "/ws/Dockerfile.esh" \
+        BASE_REVISION=${BASE_REVISION} \
+        NGINX_BASE=${NGINX_BASE} \
+        PHP_MAJOR=${PHP_MAJOR} \
+        PHP_MINOR=${V}
+    )
+
+    echo "${DOCKERFILE}" > ./${V}/Dockerfile
+
+done
+
+docker system prune -f
+echo "Done."
