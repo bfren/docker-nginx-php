bef95175cfc24b90204668503d68d349cb8d9b8c
bfren <bf@bfren.dev>
2022-09-30 11:53:54 +0100

    v5.3.0 (#129)
    Major updates
    * Removing PHP 7.3 (images still exist in 5.2.x releases)
    
    Minor updates
    * Overriding apk repositories to install latest PHP versions
    * Updating to PHP 7.4.32, 8.0.23 and 8.1.11
    
    Development updates
    * Don't warn for bare URLs

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -7,7 +7,7 @@ docker pull bfren/alpine
 BASE_REVISION="4.0.15"
 echo "Base: ${BASE_REVISION}"
 
-PHP_VERSIONS="7.3 7.4 8.0 8.1"
+PHP_VERSIONS="7.4 8.0 8.1 8.2"
 for V in ${PHP_VERSIONS} ; do
 
     echo "PHP ${V}"
@@ -17,6 +17,10 @@ for V in ${PHP_VERSIONS} ; do
     if [ "${V}" = "8.1" ] ; then
         PHP_DIR="/etc/php81"
         PHP_INI_ERROR_LOG="/var/log/php81/error.log"
+
+    elif [ "${V}" = "8.2" ] ; then
+        PHP_DIR="/etc/php82"
+        PHP_INI_ERROR_LOG="/var/log/php82/error.log"
     else
         PHP_DIR="/etc/php${PHP_MAJOR}"
         PHP_INI_ERROR_LOG="/var/log/php${PHP_MAJOR}/error.log"
