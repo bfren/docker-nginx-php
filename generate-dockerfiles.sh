6baf1717cc59b14cdee992a7cc22623e23cf1caf
bfren <bf@bfren.dev>
2025-01-31 10:37:14 +0000

    v7.3.3 (#227)
    Minor updates
    * Using latest base images
    * Updating to PHP 8.3.16 and 8.4.3
    
    Documentation updates
    * Updating copyright year to 2025

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_VERSION="6.4.1"
-PHP_BRANCH="v2.3.2"
+BASE_VERSION="6.4.2"
+PHP_BRANCH="v2.3.3"
 PHP_VERSIONS="7.4 8.0 8.1 8.2 8.3 8.4"
 
 for V in ${PHP_VERSIONS} ; do
