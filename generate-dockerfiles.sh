0e4e6273e31317aadc2f7b6fc4a44beec621e58b
bfren <bf@bfren.dev>
2025-06-06 15:10:35 +0100

    v7.4.0 (#232)
    Major updates
    * Using latest base images
    * Switching to Alpine 3.22 and Nginx 1.28.0 for PHP 8.3 and 8.4
    
    Minor updates
    * Updating PHP 8.2 build and to PHP 8.3.22 and 8.4.8
    
    Build updates
    * Using GitHub CLI for auto PR

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_VERSION="6.4.5"
-PHP_BRANCH="v2.3.6"
+BASE_VERSION="6.5.1"
+PHP_BRANCH="v2.4.0"
 PHP_VERSIONS="7.4 8.0 8.1 8.2 8.3 8.4"
 
 for V in ${PHP_VERSIONS} ; do
