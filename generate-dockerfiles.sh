d5e74f8cb689090fa75891900602cbc48ca692f6
bfren <bf@bfren.dev>
2026-03-29 13:18:17 +0100

    Using latest base images

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 docker pull bfren/alpine
 
 BASE_VERSION="7.1.4"
-PHP_BRANCH="v3.1.6"
+PHP_BRANCH="v3.1.7"
 PHP_VERSIONS="7.4 8.0 8.1 8.2 8.3 8.4 8.5"
 
 for V in ${PHP_VERSIONS} ; do
